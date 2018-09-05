d4035d502ae6dcb739e4596642118b84b6a6dc01
Måns Magnusson <exoji2e@gmail.com>
2018-09-05 23:36:11 +0200

    update printing and enable usage for other kommuner

--- extract.py
+++ extract.py
@@ -2,9 +2,20 @@ from urllib import request
 import lxml.html
 from lxml import etree
 from collections import defaultdict
+import sys
 
+kommun = {'lund' : '12/81', 'svalov' : '12/14', 'eslov':'12/85'}
 root = 'https://data.val.se/val/val2018/rostmottagning/vallokal/'
-lund = root + 'kommun/12/81/index.html'
+
+if len(sys.argv) > 1 and sys.argv[1] in kommun:
+    target = root + 'kommun/' + kommun[sys.argv[1]] + '/index.html'
+    print(sys.argv[1])
+else:
+    target = root + 'kommun/12/81/index.html'
+    print('lund')
+
+
+open_str = '\nÖppet på valdagen 9 september 2018'
 
 def get_info(url):
     r = request.urlopen(url)
@@ -16,9 +27,9 @@ def get_info(url):
         ps = []
         for p in info.xpath('p/text()'):
             ps.append(p)
-        if ps[2] != '\nÖppet på valdagen 9 september 2018':
+        if ps[2] != open_str:
             print(ps)
-        return (name, ps[1], ps[0])
+        return (name, '' if ps[1] == open_str else ps[1], ps[0])
         #return etree.tostring(info, pretty_print=True, encoding='unicode')
     return None
 
@@ -45,7 +56,7 @@ def get_districts(url):
 
     return urls
 
-urls = get_districts(lund)
+urls = get_districts(target)
 
 d = defaultdict(list)
 for name, url in urls.items():
@@ -53,7 +64,9 @@ for name, url in urls.items():
     name, addr, entr = get_info(vallokal_url)
     d[name, addr].append(entr)
     print(name)
+
+print('antal valkretsar; namn på vallokal; adress; eventuell beskrivning av vilka rum')
 for (name, addr), entrances in sorted(d.items()):
-    print(len(entrances), name, addr, ':', ', '.join(entrances))
+    print('; '.join(map(str,[len(entrances), name, addr] + entrances)))
     
 
